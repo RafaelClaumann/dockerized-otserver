678837a113de47857ff3971148083e6afc34955a
RafaelClaumann <rafael_claumann@hotmail.com>
2023-04-04 13:25:57 -0300

    criando conta e chars no data.sql e update README
    A conta @a/1 recebera quatro personagens quando mysql
    for iniciado.

--- site/login.php
+++ site/login.php
@@ -83,7 +83,11 @@ function create_char($player, $highestLevelId) {
 		"1" => "Sorcerer",
 		"2" => "Druid",
 		"3" => "Paladin",
-		"4" => "Knight"
+		"4" => "Knight",
+		"5" => "Master Sorcerer",
+		"6" => "Elder Druid",
+		"7" => "Royal Paladin",
+		"8" => "Elite Knight"
 	];
 
 	return [
